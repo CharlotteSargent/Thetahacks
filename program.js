3940157c85ba83ef0164f16e92afb2a88c11601b
Khaylah <46227207+roxie07nerd@users.noreply.github.com>
2021-01-16 13:05:00 -0500

    Update program.js
    Fixed screen issue just placing the html i image then deleting the js one
     my laptop is dying so  uploadig.

--- program.js
+++ program.js
@@ -1,11 +1,30 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+//canvas bg
+ctx.fillStyle = "grey";
+ctx.fillRect(0, 0, cvs.width, cvs.height);
+//mages
+var bgs = new Image();
+bgs.id = "bgt";
+bgs.src = "images/bgt.png";
 
+	//rect
+	ctx.beginPath();
+	ctx.lineWidth = "6";
+	ctx.strokeStyle = "white";
+	ctx.rect(270,70,500,100);
+	ctx.stroke();
+	
+	bgImageStart();
 function startScreen(){
+	//text
     ctx.font = "30px Comic Sans MS";
     ctx.fillStyle = "red";
     ctx.fillText("Mystery", 200,100);
+
+	
+	//input
     var na = document.createElement("input");
     na.setAttribute("id", "name");
     na.setAttribute("type", "text");
@@ -20,22 +39,29 @@ function startScreen(){
         console.log(name);
     };
     //document.getElementById("name").style.
+	
 }
 
 
+function bgImageStart(){
+	ctx.drawImage(bgs,-150,100,500,500);
+	requestAnimationFrame(bgImageStart);
 
+}
 function start(){
-
+  	ctx.clearRect(0, 0, cvs.width, cvs.height);
     	
 }
 function remove() {
-    var elem = document.getElementById('startBtn');
+    var elem = document.getElementById('container');	 
     elem.parentNode.removeChild(elem);
+	start();
     return false;
 }
 function pageInit() {
     // Hook up the "remove dummy" button
     var btn = document.getElementById('startBtn');
+	
     if (btn.addEventListener){
         // DOM2 standard
         btn.addEventListener('click', remove, false);
