220bc2e5861761a74688e5ba0f2a77daad2c981e
Khaylah <46227207+roxie07nerd@users.noreply.github.com>
2021-01-15 22:54:22 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,1 +1,16 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+
+
+function startScreen(){
+
+ctx.font = "30px Comic Sans MS";
+ctx.fillStyle = "red";
+ctx.fillText("Mystery", 200,100);
+}
+
+function start(){
+	
+}
 
